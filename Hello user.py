229dcd688fede09404fd180f0e44e037df73cc52
Joe Grant <128994377+josephgrant666@users.noreply.github.com>
2023-10-12 22:28:39 +0100

    Add files via upload

---
+++ Hello user.py
@@ -0,0 +1,10 @@
+print('Hello world')
+
+print('What is your name?')
+myName = input()
+print('Your name is ' + str(len(myName)) + ' letters long')
+
+print('How old are you?')
+myAge = input()
+
+print('You will be ' + str(int(myAge) + 1) + ' the next time you celebrate your birthday')
