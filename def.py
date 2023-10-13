3145245387d2312bbb59e40e2ad00ebc0fb0b1fb
Joe Grant <128994377+josephgrant666@users.noreply.github.com>
2023-10-13 23:33:55 +0100

    Add files via upload

---
+++ def.py
@@ -0,0 +1,13 @@
+def hello():
+    print('Hello')
+    print('Hello Joe')
+    print('Hi there')
+
+hello()
+
+def hello(name):
+    print('Hello ' + name)
+
+hello('Joe')
+
+print('Hello has ' + str(len('Hello')) + ' letters in it.')
