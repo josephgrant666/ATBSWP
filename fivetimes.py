ee2452e7687d20c39f91f364a4bc932bbe75c62b
Joe Grant <128994377+josephgrant666@users.noreply.github.com>
2023-10-29 13:53:40 +0000

    Add files via upload

---
+++ fivetimes.py
@@ -0,0 +1,15 @@
+print('My name is')
+for i in range(5):
+    print('Joe five times ' + str(i))
+
+# Gauss' calculation 
+total = 0 
+print('All of the numbers between 1 and 100 added together are equal to...')
+for num in range(101):
+    total = total + num 
+print(total)
+
+i = 0 
+while i < 5:
+    print ('Joe five times ' + str(i))
+    i = i + 1 
